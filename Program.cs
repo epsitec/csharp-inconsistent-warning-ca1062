69087775edbde40558b770540e30f92937dc86e3
Pierre Arnaud <arnaud@epsitec.ch>
2021-05-16 06:48:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//	Copyright © 2021, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace InconsistentWarning
+{
+    class Program
+    {
+        static void Main()
+        {
+            _ = new Foo ("Hello");
+        }
+    }
+
+    public class Foo
+    {
+        public Foo(string x)
+        {
+            this.x = x; // does not warn
+            this.len = x.Length; // warns (OK) -- CA1062
+        }
+
+        public void SetX(string x)
+        {
+            this.x = x; // does not warn
+            this.len = x.Length; // warns (OK) -- CA1062
+        }
+
+        public string X => this.x;
+        public int Len => this.len;
+
+        private string x;
+        private int len;
+    }
+}
